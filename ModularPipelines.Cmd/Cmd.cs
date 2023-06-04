ff129363da8cc5c9d62088f803a94011bf06a0e3
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2023-06-04 21:14:22 +0100

    Missed files

---
+++ ModularPipelines.Cmd/Cmd.cs
@@ -0,0 +1,31 @@
+﻿using CliWrap.Buffered;
+using ModularPipelines.Cmd.Models;
+using ModularPipelines.Command.Extensions;
+using ModularPipelines.Command.Options;
+using ModularPipelines.Context;
+
+namespace ModularPipelines.Cmd;
+
+public class Cmd<T> : ICmd<T>
+{
+    private readonly IModuleContext<T> _context;
+
+    public Cmd(IModuleContext<T> context)
+    {
+        _context = context;
+    }
+    
+    public Task<BufferedCommandResult> Script(CmdScriptOptions options, CancellationToken cancellationToken = default)
+    {
+        var arguments = new List<string> { "/c" };
+
+        if (!options.Echo)
+        {
+            arguments.Add("/q");
+        }
+        
+        arguments.Add(options.Script);
+
+        return _context.Command().UsingCommandLineTool(options.ToCommandLineToolOptions("cmd", arguments), cancellationToken);
+    }
+}
---
+++ ModularPipelines.Node/Extensions/NodeExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using ModularPipelines.Command.Extensions;
+using ModularPipelines.Context;
+using ModularPipelines.Engine;
+
+namespace ModularPipelines.Node.Extensions;
+
+public static class NodeExtensions
+{
+#pragma warning disable CA2255
+    [ModuleInitializer]
+#pragma warning restore CA2255
+    public static void RegisterNodeContext()
+    {
+        ServiceContextRegistry.RegisterContext(collection => RegisterNodeContext(collection));
+    }
+    
+    public static IServiceCollection RegisterNodeContext(this IServiceCollection services)
+    {
+        services.RegisterCommandContext();
+        services.TryAddSingleton(typeof(INode<>), typeof(Node<>));
+        services.TryAddSingleton(typeof(INvm<>), typeof(Nvm<>));
+        services.TryAddSingleton(typeof(INpm<>), typeof(Npm<>));
+        return services;
+    }
+    
+    public static INode Node(this IModuleContext context) => (INode) context.ServiceProvider.GetRequiredService(typeof(INode<>).MakeGenericType(context.ModuleType));
+}
