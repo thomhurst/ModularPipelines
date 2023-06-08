2d16444399eb28c2123200820e17b8789127bf99
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2023-06-08 21:43:22 +0100

    Remove GitVersion

--- ModularPipelines.Git/GitInformation.cs
+++ ModularPipelines.Git/GitInformation.cs
@@ -1,16 +1,27 @@
+using ModularPipelines.Context;
+using ModularPipelines.Options;
+
 namespace ModularPipelines.Git;
 
 public class GitInformation<T> : IGitInformation<T>
 {
-    public string BranchName => GitVersionInformation.BranchName;
-    public string SemanticVersion => GitVersionInformation.SemVer;
-    public string MajorMinorPatch => GitVersionInformation.MajorMinorPatch;
-    public string Major => GitVersionInformation.Major;
-    public string Minor => GitVersionInformation.Minor;
-    public string Patch => GitVersionInformation.Patch;
+    private readonly IModuleContext<T> _context;
+
+    public GitInformation(IModuleContext<T> context)
+    {
+        _context = context;
+    }
+    
+    public string BranchName => RunCommands(new []{ "rev-parse", "--abbrev-ref", "HEAD" });term
     public string Tag => GitVersionInformation.PreReleaseTag;
     public string Label => GitVersionInformation.PreReleaseLabel;
     public int CommitsOnBranch => int.Parse(GitVersionInformation.CommitsSinceVersionSource);
     public DateOnly CommitDate => DateOnly.Parse(GitVersionInformation.CommitDate);
     public string Sha => GitVersionInformation.Sha;
+
+    private string RunCommands(IEnumerable<string> commands) => _context.Command.UsingCommandLineTool(
+        new CommandLineToolOptions("git")
+        {
+            Arguments = commands
+        }).Result.StandardOutput;
 }
