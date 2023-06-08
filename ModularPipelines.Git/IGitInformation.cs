0ba1c9531d55b05b490e73b79b3926e858ac0fde
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2023-06-08 23:28:27 +0100

    Nullable

--- ModularPipelines.Git/IGitInformation.cs
+++ ModularPipelines.Git/IGitInformation.cs
@@ -2,13 +2,13 @@ namespace ModularPipelines.Git;
 
 public interface IGitInformation
 {
-    public string BranchName { get; }
-    public string DefaultBranchName { get; }
-    public string LastCommitSha { get; }
-    public string LastCommitShortSha { get; }
+    public string? BranchName { get; }
+    public string? DefaultBranchName { get; }
+    public string? LastCommitSha { get; }
+    public string? LastCommitShortSha { get; }
 
-    public string Tag { get; }
-    public string LastCommit { get; }
+    public string? Tag { get; }
+    public string? LastCommit { get; }
     public Task<IEnumerable<string>> LastCommits(int count);
     public int CommitsOnBranch { get; }
     public DateTimeOffset LastCommitDateTime { get; }
--- ModularPipelines.Git/StaticGitInformation.cs
+++ ModularPipelines.Git/StaticGitInformation.cs
@@ -24,14 +24,14 @@ internal class StaticGitInformation : IGitInformation<StaticGitInformation>, IIn
             throw new Exception("Error detecting Git repository", e);
         }
         
-        BranchName = await RunCommands("rev-parse", "--abbrev-ref", "HEAD");
-        DefaultBranchName = (await RunCommands("rev-parse", "--abbrev-ref", "origin/HEAD")).Replace("origin/", string.Empty);
-        LastCommit = await RunCommands("log", "-1");
-        LastCommitSha = await RunCommands("rev-parse", "HEAD");
-        LastCommitShortSha = await  RunCommands("rev-parse", "--short", "HEAD");
-        Tag =  await RunCommands("describe", "--tags");
-        CommitsOnBranch =  int.Parse(await RunCommands("rev-list", "HEAD", "--count"));
-        LastCommitDateTime = DateTimeOffset.FromUnixTimeSeconds(long.Parse(await RunCommands("log", "-1", "--format=%at")));
+        BranchName = await RunCommandsOrNull("rev-parse", "--abbrev-ref", "HEAD");
+        DefaultBranchName = (await RunCommandsOrNull("rev-parse", "--abbrev-ref", "origin/HEAD")).Replace("origin/", string.Empty);
+        LastCommit = await RunCommandsOrNull("log", "-1");
+        LastCommitSha = await RunCommandsOrNull("rev-parse", "HEAD");
+        LastCommitShortSha = await  RunCommandsOrNull("rev-parse", "--short", "HEAD");
+        Tag =  await RunCommandsOrNull("describe", "--tags");
+        CommitsOnBranch =  int.Parse(await RunCommandsOrNull("rev-list", "HEAD", "--count") ?? "0");
+        LastCommitDateTime = DateTimeOffset.FromUnixTimeSeconds(long.Parse(await RunCommandsOrNull("log", "-1", "--format=%at") ?? "0"));
     }
     
     private async Task<string> RunCommands(params string[] commands)
@@ -44,13 +44,25 @@ internal class StaticGitInformation : IGitInformation<StaticGitInformation>, IIn
 
         return commandResult.StandardOutput.Trim();
     }
+
+    private async Task<string?> RunCommandsOrNull(params string[] commands)
+    {
+        try
+        {
+            return await RunCommands(commands);
+        }
+        catch
+        {
+            return null;
+        }
+    }
     
-    public string BranchName { get; private set; } = null!;
-    public string DefaultBranchName { get; private set; } = null!;
+    public string? BranchName { get; private set; } = null!;
+    public string? DefaultBranchName { get; private set; } = null!;
 
-    public string Tag { get; private set; } = null!;
+    public string? Tag { get; private set; } = null!;
 
-    public string LastCommit { get; private set; } = null!;
+    public string? LastCommit { get; private set; } = null!;
 
     public async Task<IEnumerable<string>> LastCommits(int count)
     {
@@ -61,7 +73,7 @@ internal class StaticGitInformation : IGitInformation<StaticGitInformation>, IIn
     public int CommitsOnBranch { get; private set; }
     public DateTimeOffset LastCommitDateTime { get; private set; }
         
-    public string LastCommitSha { get; private set; } = null!;
+    public string? LastCommitSha { get; private set; } = null!;
 
-    public string LastCommitShortSha { get; private set; } = null!;
+    public string? LastCommitShortSha { get; private set; } = null!;
 }
