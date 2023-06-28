5a645724d99dfa48986996e3f1dc97276b39967f
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2023-06-28 21:17:15 +0100

    +semver: feature

--- ModularPipelines.Build/Modules/NugetVersionGeneratorModule.cs
+++ ModularPipelines.Build/Modules/NugetVersionGeneratorModule.cs
@@ -10,6 +10,11 @@ public class NugetVersionGeneratorModule : Module<string>
     protected override async Task<ModuleResult<string>?> ExecuteAsync(IModuleContext context, CancellationToken cancellationToken)
     {
         await Task.Yield();
+
+        if (GitVersionInformation.BranchName == "main")
+        {
+            return $"{GitVersionInformation.Major}.{GitVersionInformation.Minor}.{GitVersionInformation.Patch}";
+        }
         
         return $"{GitVersionInformation.Major}.{GitVersionInformation.Minor}.{GitVersionInformation.Patch}-{GitVersionInformation.PreReleaseLabel}-{GitVersionInformation.CommitsSinceVersionSource}";
     }
