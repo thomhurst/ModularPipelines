3758dc3d8616f613379784f3a84bc9ccc8a0f15a
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2023-07-10 20:36:55 +0100

    if(!_disposed)

--- ModularPipelines/Engine/EngineCancellationToken.cs
+++ ModularPipelines/Engine/EngineCancellationToken.cs
@@ -2,16 +2,30 @@
 
 internal class EngineCancellationToken : CancellationTokenSource
 {
+    private bool _disposed;
+
     public EngineCancellationToken()
     {
         Console.CancelKeyPress += (_, _) =>
         {
-            Cancel();
+            if(!_disposed)
+            {
+                Cancel();
+            }
         };
 
         AppDomain.CurrentDomain.ProcessExit += (_, _) =>
         {
-            Cancel();
+            if(!_disposed)
+            {
+                Cancel();
+            }
         };
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        _disposed = true;
+        base.Dispose(disposing);
     }
 }
