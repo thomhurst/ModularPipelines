51c36fae4f6f25ebcf4d39a7561e37f4c223edae
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2023-08-07 20:39:24 +0100

    Node installers

--- ModularPipelines/Context/IPredefinedInstallers.cs
+++ ModularPipelines/Context/IPredefinedInstallers.cs
@@ -6,4 +6,6 @@ public interface IPredefinedInstallers
 {
     Task<CommandResult> Chocolatey();
     Task<CommandResult> Powershell7();
+    Task<CommandResult> Nvm(string? version = null);
+    Task<CommandResult> Node(string version = "--lts");
 }
