13e469291b13dd4320aa3b1cb619e13be91cf6b3
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2023-09-04 20:05:33 +0100

    ExcludeFromCodeCoverageAttributeChanger

--- src/ModularPipelines/Attributes/ExcludeFromCodeCoverageAttribute.cs
+++
@@ -1,7 +0,0 @@
-﻿// ReSharper disable once CheckNamespace
-namespace System.Diagnostics.CodeAnalysis;
-
-[AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Constructor | AttributeTargets.Event | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Struct, AllowMultiple=false, Inherited=true)]
-public class ExcludeFromCodeCoverageAttribute : Attribute
-{
-}
