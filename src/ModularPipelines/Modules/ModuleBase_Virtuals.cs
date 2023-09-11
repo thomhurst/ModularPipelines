f7ef4687260996a62c4fed816462ab9592556115
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2023-09-11 17:49:18 +0200

    +semver:major: Retries (#126)
    * Retries

---
+++ src/ModularPipelines/Modules/ModuleBase_Virtuals.cs
@@ -0,0 +1,48 @@
+﻿using ModularPipelines.Context;
+using ModularPipelines.Engine;
+using ModularPipelines.Models;
+
+namespace ModularPipelines.Modules;
+
+public partial class ModuleBase
+{
+    /// <summary>
+    /// A Timeout for the module
+    /// </summary>
+    protected virtual TimeSpan Timeout => TimeSpan.FromMinutes(30);
+
+    /// <summary>
+    /// If true, the pipeline will not fail is this module fails.
+    /// </summary>
+    /// <param name="context"></param>
+    /// <param name="exception"></param>
+    /// <returns></returns>
+    protected virtual Task<bool> ShouldIgnoreFailures(IPipelineContext context, Exception exception) => Task.FromResult(false);
+
+    /// <summary>
+    /// If true, this module will not run.
+    /// </summary>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    protected virtual Task<bool> ShouldSkip(IPipelineContext context) => Task.FromResult(false);
+
+    /// <summary>
+    /// If this module is skipped, and this returns true, the result of this module will be reconstructed from the plugged in <see cref="IModuleResultRepository"/>.
+    /// If no persisted result can be reconstructed, this module will fail.
+    /// </summary>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    protected virtual Task<bool> UseResultFromHistoryIfSkipped(IPipelineContext context) => Task.FromResult(context.ModuleResultRepository.GetType() != typeof(NoOpModuleResultRepository));
+
+    public virtual ModuleRunType ModuleRunType => ModuleRunType.OnSuccessfulDependencies;
+    
+    protected virtual Task OnBeforeExecute(IPipelineContext context)
+    {
+        return Task.CompletedTask;
+    }
+
+    protected virtual Task OnAfterExecute(IPipelineContext context)
+    {
+        return Task.CompletedTask;
+    }
+}
