f5b36cacaaf04e0bd9ed71b6a3b5f03e4b1c6dfc
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2023-09-11 21:18:04 +0100

    , Repeat(10)

--- test/ModularPipelines.UnitTests/Helpers/DotNetTestResultsTests.cs
+++ test/ModularPipelines.UnitTests/Helpers/DotNetTestResultsTests.cs
@@ -46,7 +46,7 @@ public class DotNetTestResultsTests : TestBase
         }
     }
 
-    [Test]
+    [Test, Repeat(10)]
     public void Has_Errored()
     {
         var moduleFailedException = Assert.ThrowsAsync<ModuleFailedException>(async () => await RunModule<DotNetTestWithFailureModule>())!;
@@ -67,7 +67,7 @@ public class DotNetTestResultsTests : TestBase
         });
     }
     
-    [Test]
+    [Test, Repeat(10)]
     public async Task Has_Not_Errored()
     {
         var module = await RunModule<DotNetTestWithoutFailureModule>();
