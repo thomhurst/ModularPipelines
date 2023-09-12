0b482b1b65918e2e12726620861d70cffd827ad3
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2023-09-12 16:59:58 +0100

    Try make InstallFromFile work for Linux

--- src/ModularPipelines/Context/FileInstaller.cs
+++ src/ModularPipelines/Context/FileInstaller.cs
@@ -1,4 +1,5 @@
-﻿using ModularPipelines.Models;
+﻿using System.Runtime.InteropServices;
+using ModularPipelines.Models;
 using ModularPipelines.Options;
 
 namespace ModularPipelines.Context;
@@ -7,20 +8,29 @@ public class FileInstaller : IFileInstaller
 {
     private readonly ICommand _command;
     private readonly IDownloader _downloader;
+    private readonly IBash _bash;
 
-    public FileInstaller(ICommand command, IDownloader downloader)
+    public FileInstaller(ICommand command, IDownloader downloader, IBash bash)
     {
         _command = command;
         _downloader = downloader;
+        _bash = bash;
     }
 
-    public Task<CommandResult> InstallFromFileAsync(InstallerOptions options,
+    public async Task<CommandResult> InstallFromFileAsync(InstallerOptions options,
         CancellationToken cancellationToken = default)
     {
-        return _command.ExecuteCommandLineTool(new CommandLineToolOptions(options.Path)
+        if(RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
         {
-            Arguments = options.Arguments ?? Array.Empty<string>()
-        }, cancellationToken);
+            return await _command.ExecuteCommandLineTool(new CommandLineToolOptions(options.Path)
+            {
+                Arguments = options.Arguments ?? Array.Empty<string>()
+            }, cancellationToken);
+        }
+
+        await _bash.Command(new BashCommandOptions($"chmod u+x {options.Path}"), cancellationToken);
+
+        return await _bash.FromFile(new BashFileOptions(options.Path), cancellationToken);
     }
 
     public async Task<CommandResult> InstallFromWebAsync(WebInstallerOptions options,
