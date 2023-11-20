4df762da3e9f41602ab7d3ccef5f737bc1ff7c28
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2023-11-20 20:00:30 +0000

    RunOnLinuxOnlyAttribute RunOnWindowsOnlyAttribute RunOnMacOSOnlyAttribute (#290)
    * RunOnLinuxOnlyAttribute.cs RunOnWindowsOnlyAttribute.cs RunOnMacOSOnlyAttribute.cs
    
    * Formatting Markdown
    
    ---------
    
    Co-authored-by: Tom Longhurst <thomhurst@users.noreply.github.com>

---
+++ src/ModularPipelines/Attributes/RunOnWindowsOnlyAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using ModularPipelines.Context;
+
+namespace ModularPipelines.Attributes;
+
+[ExcludeFromCodeCoverage]
+public class RunOnWindowsOnlyAttribute : MandatoryRunConditionAttribute
+{
+    /// <inheritdoc/>
+    public override Task<bool> Condition(IPipelineHookContext pipelineContext)
+    {
+        return Task.FromResult(OperatingSystem.IsWindows());
+    }
+}
