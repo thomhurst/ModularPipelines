c7b9524b36c05d7e7bf2fcabd1ec3f2377a2c99e
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2023-11-22 13:59:57 +0000

    Missed the INvm interface

--- src/ModularPipelines.Node/INvm.cs
+++ src/ModularPipelines.Node/INvm.cs
@@ -5,4 +5,7 @@ namespace ModularPipelines.Node;
 public interface INvm
 {
     Task<CommandResult> Use(string version, CancellationToken cancellationToken = default);
+    Task<CommandResult> Install(string version, CancellationToken cancellationToken = default);
+    Task<CommandResult> Version(CancellationToken cancellationToken = default);
+    Task<CommandResult> Which(CancellationToken cancellationToken = default);
 }
