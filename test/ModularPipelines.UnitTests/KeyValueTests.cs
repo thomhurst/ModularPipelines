1a7643da558d3c16669c6a172b52b18c99435e8a
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2023-12-23 00:38:01 +0000

    KeyValue implicit operators (#346)
    * KeyValue implicit operators
    
    * Formatting Markdown
    
    ---------
    
    Co-authored-by: Tom Longhurst <thomhurst@users.noreply.github.com>

---
+++ test/ModularPipelines.UnitTests/KeyValueTests.cs
@@ -0,0 +1,42 @@
+﻿using ModularPipelines.Models;
+
+namespace ModularPipelines.UnitTests;
+
+public class KeyValueTests
+{
+    [Test]
+    public void ImplicitOperator1()
+    {
+        KeyValue keyValue = ("one", "two");
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(keyValue.Key, Is.EqualTo("one"));
+            Assert.That(keyValue.Value, Is.EqualTo("two"));
+        });
+    }
+    
+    [Test]
+    public void ImplicitOperator2()
+    {
+        KeyValue keyValue = new Tuple<string, string>("one", "two");
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(keyValue.Key, Is.EqualTo("one"));
+            Assert.That(keyValue.Value, Is.EqualTo("two"));
+        });
+    }
+    
+    [Test]
+    public void ImplicitOperator3()
+    {
+        KeyValue keyValue = new KeyValuePair<string, string>("one", "two");
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(keyValue.Key, Is.EqualTo("one"));
+            Assert.That(keyValue.Value, Is.EqualTo("two"));
+        });
+    }
+}
