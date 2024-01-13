e95037833a9103e10b265d1201c47646483d6bdc
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-01-13 14:46:32 +0000

    DependencyFailedException GetInnerMostFailingModule (#376)
    * DependencyFailedException GetInnerMostFailingModule
    
    * FailingModuleName
    
    * Formatting Markdown
    
    ---------
    
    Co-authored-by: Tom Longhurst <thomhurst@users.noreply.github.com>

--- src/ModularPipelines/Exceptions/DependencyFailedException.cs
+++ src/ModularPipelines/Exceptions/DependencyFailedException.cs
@@ -1,10 +1,34 @@
+using System.Text.Json.Serialization;
 using ModularPipelines.Modules;
 
 namespace ModularPipelines.Exceptions;
 
 public class DependencyFailedException : PipelineException
 {
-    public DependencyFailedException(Exception exception, ModuleBase moduleBase) : base($"The dependency {moduleBase.GetType().Name} has failed.", exception)
+    [JsonInclude]
+    public string FailingModuleName { get; private set; }
+
+    public DependencyFailedException(Exception exception, ModuleBase moduleBase) : base($"The dependency {GetInnerMostFailingModule(moduleBase, exception)} has failed.", exception)
     {
+        FailingModuleName = moduleBase.GetType().Name;
+    }
+    
+    private static string GetInnerMostFailingModule(ModuleBase rootModuleBase, Exception rootException)
+    {
+        var module = rootModuleBase.GetType().Name;
+        
+        var exception = rootException;
+        
+        while (exception != null)
+        {
+            if (exception is DependencyFailedException dependencyFailedException)
+            {
+                module = dependencyFailedException.FailingModuleName;
+            }
+
+            exception = exception.InnerException;
+        }
+
+        return module;
     }
 }
