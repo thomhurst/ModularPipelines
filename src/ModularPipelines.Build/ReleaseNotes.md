b69904876016d413b7f5923f502e1b9300fa9ce0
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-01-13 13:47:52 +0000

    DependencyFailedException GetInnerMostFailingModule

--- src/ModularPipelines.Build/ReleaseNotes.md
+++ src/ModularPipelines.Build/ReleaseNotes.md
@@ -1,1 +1,1 @@
-null
+- DependencyFailedException shows the root module that failed if it's part of a chain of dependent modules
--- src/ModularPipelines/Exceptions/DependencyFailedException.cs
+++ src/ModularPipelines/Exceptions/DependencyFailedException.cs
@@ -4,7 +4,29 @@ namespace ModularPipelines.Exceptions;
 
 public class DependencyFailedException : PipelineException
 {
-    public DependencyFailedException(Exception exception, ModuleBase moduleBase) : base($"The dependency {moduleBase.GetType().Name} has failed.", exception)
+    private readonly ModuleBase _moduleBase;
+
+    public DependencyFailedException(Exception exception, ModuleBase moduleBase) : base($"The dependency {GetInnerMostFailingModule(moduleBase, exception).GetType().Name} has failed.", exception)
     {
+        _moduleBase = moduleBase;
+    }
+    
+    private static ModuleBase GetInnerMostFailingModule(ModuleBase rootModuleBase, Exception rootException)
+    {
+        var module = rootModuleBase;
+        
+        var exception = rootException;
+        
+        while (exception != null)
+        {
+            if (exception is DependencyFailedException dependencyFailedException)
+            {
+                module = dependencyFailedException._moduleBase;
+            }
+
+            exception = exception.InnerException;
+        }
+
+        return module;
     }
 }
