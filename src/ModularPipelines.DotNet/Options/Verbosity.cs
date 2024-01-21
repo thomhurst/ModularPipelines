4fd1ab517fc21808a815369e495986f492feec8a
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-01-21 15:44:58 +0000

    Fix

--- src/ModularPipelines.DotNet/Options/Verbosity.cs
+++ src/ModularPipelines.DotNet/Options/Verbosity.cs
@@ -2,13 +2,13 @@ namespace ModularPipelines.DotNet.Options;
 
 public static class Verbosity
 {
-    public static readonly string Quiet = "Quiet";
+    public static readonly string Quiet = "quiet";
 
-    public static readonly string Minimal = "Minimal";
+    public static readonly string Minimal = "minimal";
 
-    public static readonly string Normal = "Normal";
+    public static readonly string Normal = "normal";
 
-    public static readonly string Detailed = "Detailed";
+    public static readonly string Detailed = "detailed";
 
-    public static readonly string Diagnostic = "Diagnostic";
+    public static readonly string Diagnostic = "diagnostic";
 }
