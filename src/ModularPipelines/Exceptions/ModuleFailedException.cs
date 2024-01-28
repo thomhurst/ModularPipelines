b8edd5727219686e256c10f95252aae6f3457716
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-01-28 15:44:57 +0000

    Improve message within ModuleFailedException

--- src/ModularPipelines/Exceptions/ModuleFailedException.cs
+++ src/ModularPipelines/Exceptions/ModuleFailedException.cs
@@ -6,8 +6,18 @@ public class ModuleFailedException : PipelineException
 {
     public ModuleBase Module { get; }
 
-    public ModuleFailedException(ModuleBase module, Exception exception) : base($"The module {module.GetType().Name} has failed.", exception)
+    public ModuleFailedException(ModuleBase module, Exception exception) : base($"The module {module.GetType().Name} has failed.{GetInnerMessage(exception)}", exception)
     {
         Module = module;
+    }
+
+    private static string GetInnerMessage(Exception exception)
+    {
+        if (!string.IsNullOrEmpty(exception?.Message))
+        {
+            return $"{Environment.NewLine}{Environment.NewLine}{exception.Message}";
+        }
+
+        return string.Empty;
     }
 }
