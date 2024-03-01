71a024a84843f27b1e88ec94de71daab9b66ed06
Dinko Pavicic <dinko.pavicic@gmail.com>
2024-03-01 16:51:04 +0100

    Refactor/add GitHub client (#437)
    * ref(GitHub): Add GitHub client and repository info
    
    * ref(GitHub): Refactor and polish GitHub client API and options
    
    - Add GitHub options with access token
    - Implement IIntializable for async init of GitHubRepositoryInfo
    - Add Url property to IGitHubRepositoryInfo
    - Use IGit the get remote repo data and remove LibGit2Sharp
    - Remove unneeded tuple for owner, repo
    
    * ref(GitHub): Better exception handling and minor tweaks
    
    - Change IGitHubRepositoryInfo properties to be nullable
    - Add IsInitialzied property on IGitHubRepositoryInfo to easily check the state and avoid mutiple initializations
    - Set ThrowOnNonZeroExitCode = false on GitRemoteOptions
    
    * docs(GitHub): Add documentation for GitHub package and update release notes
    
    * fix: Async services on GitHubRepositoryInfo

---
+++ src/ModularPipelines.GitHub/IGitHubRepositoryInfo.cs
@@ -0,0 +1,45 @@
+﻿namespace ModularPipelines.GitHub;
+
+public interface IGitHubRepositoryInfo
+{
+  /// <summary>
+  /// Gets a value indicating whether <see cref="IGitHubRepositoryInfo"/> has been initialized.
+  /// </summary>
+  /// <remarks>
+  /// If (remote) github repository hasn't been set, the repository info will not be initialized.
+  /// In that case, one can check if that's the case.
+  /// </remarks>
+  bool IsInitialized { get; }
+  
+  /// <summary>
+  /// Gets the URL of a GitHub repository.
+  /// </summary>
+  string? Url { get; }
+
+  /// <summary>
+  /// Gets an endpoint for accessing a remote repository.
+  /// </summary>
+  string? Endpoint { get; }
+
+  /// <summary>
+  /// Gets the owner of a repository.
+  /// </summary>
+  string? Owner { get; }
+
+  /// <summary>
+  /// Gets the name of the repository.
+  /// </summary>
+  /// <value>The name of the repository.</value>
+  string? RepositoryName { get; }
+  
+  /// <summary>
+  /// Gets the identifier of a GitHub repository.
+  /// </summary>
+  string? Identifier => $"{Owner}/{RepositoryName}";
+
+  /// <summary>
+  /// Gets a value indicating whether the endpoint is GitHub.
+  /// </summary>
+  /// <value><c>true</c> if the endpoint is GitHub; otherwise, <c>false</c>.</value>
+  bool IsGitHub => Endpoint?.Equals("github.com", StringComparison.OrdinalIgnoreCase) ?? false;
+}
