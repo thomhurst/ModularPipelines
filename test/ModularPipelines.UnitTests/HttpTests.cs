00aca516c1a3c88b0e2c058e6b35c50742ea6238
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-03-26 14:26:25 +0000

    Fix

--- test/ModularPipelines.UnitTests/HttpTests.cs
+++ test/ModularPipelines.UnitTests/HttpTests.cs
@@ -41,7 +41,7 @@ public class HttpTests : TestBase
         await result.Host.DisposeAsync();
 
         var logFile = await File.ReadAllTextAsync(file);
-        await Assert.That(logFile).Does.Contain("INFO	[ModularPipelines.Http.ResponseLoggingHttpHandler]");
+        await Assert.That(logFile).Does.Contain("INFO	[ModularPipelines.Http");
         await Assert.That(logFile).Does.Not.Contain("---Request---");
         await Assert.That(logFile).Does.Not.Contain("GET https://www.github.com/ HTTP/1.1");
         await Assert.That(logFile).Does.Contain("---Response---");
