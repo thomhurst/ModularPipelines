7bdaa2204820c142429dde9c1c0cd2416f6cc3bb
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-05-16 12:22:17 +0100

    Global Usings.cs

--- test/ModularPipelines.Azure.UnitTests/AzureCommandTests.cs
+++ test/ModularPipelines.Azure.UnitTests/AzureCommandTests.cs
@@ -4,7 +4,6 @@ using ModularPipelines.Context;
 using ModularPipelines.Models;
 using ModularPipelines.Modules;
 using ModularPipelines.TestHelpers;
-using TUnit.Assertions.Extensions;
 
 namespace ModularPipelines.UnitTests;
 
--- test/ModularPipelines.Azure.UnitTests/Usings.cs
+++ test/ModularPipelines.Azure.UnitTests/Usings.cs
@@ -1,1 +1,3 @@
-global using NUnit.Framework;
+global using TUnit.Core;
+global using TUnit.Assertions.Extensions;
+global using TUnit.Assertions;
