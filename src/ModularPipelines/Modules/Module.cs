13530476164cc8ace3858a13508fb86d8d97afa3
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-05-16 17:49:11 +0100

    Tidy code

--- src/ModularPipelines/Modules/Module.cs
+++ src/ModularPipelines/Modules/Module.cs
@@ -3,7 +3,6 @@ using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.Xml.Schema;
 using Microsoft.Extensions.Logging;
 using ModularPipelines.Attributes;
 using ModularPipelines.Context;
