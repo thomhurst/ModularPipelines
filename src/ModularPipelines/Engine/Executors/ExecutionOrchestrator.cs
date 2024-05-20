22d286040b808e789ea4ffb41e595d1990317511
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-05-20 18:48:37 +0100

    _waitOrchestrator.NotifyFinish();

--- src/ModularPipelines/Engine/Executors/ExecutionOrchestrator.cs
+++ src/ModularPipelines/Engine/Executors/ExecutionOrchestrator.cs
@@ -107,11 +107,14 @@ internal class ExecutionOrchestrator : IExecutionOrchestrator
                 _logger.LogInformation("Cancellation Reason: {Reason}", _engineCancellationToken.Reason);
             }
         
+            _waitOrchestrator.NotifyFinish();
+            
             return pipelineSummary;
         }
-        finally
+        catch
         {
             _waitOrchestrator.NotifyFinish();
+            throw;
         }
     }
 
