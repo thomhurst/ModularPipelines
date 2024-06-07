153ef4d4a6fcc05c27a1146d01d92b3a443ef856
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-06-07 12:03:32 +0100

    IsOrInheritsFrom

---
+++ src/ModularPipelines/Extensions/TypeExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace ModularPipelines.Extensions;
+
+internal static class TypeExtensions
+{
+    public static bool IsOrInheritsFrom(this Type type, Type otherType)
+    {
+        return type == otherType || type.IsSubclassOf(otherType);
+    }
+}
--- src/ModularPipelines/Modules/Module.cs
+++ src/ModularPipelines/Modules/Module.cs
@@ -38,7 +38,7 @@ public abstract partial class Module<T> : ModuleBase<T>
         foreach (var customAttribute in GetType().GetCustomAttributesIncludingBaseInterfaces<DependsOnAllModulesInheritingFromAttribute>())
         {
             var types = Context.ServiceProvider.GetServices<ModuleBase>()
-                .Where(x => x.GetType() == customAttribute.Type);
+                .Where(x => x.GetType().IsOrInheritsFrom(customAttribute.Type));
             
             foreach (var moduleBase in types)
             {
