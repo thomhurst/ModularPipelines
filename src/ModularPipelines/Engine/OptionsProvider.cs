6fa1436f08d260bcf818dc92ecf01bc690d1c0de
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-07-14 00:35:22 +0100

    Fix array

--- src/ModularPipelines/Engine/OptionsProvider.cs
+++ src/ModularPipelines/Engine/OptionsProvider.cs
@@ -35,7 +35,7 @@ internal class OptionsProvider : IOptionsProvider
             .Select(s => s.GetGenericArguments()[0])
             .ToList();
 
-        foreach (var option in types.Select(t => _serviceProvider.GetService(typeof(IOptions<>).MakeGenericType([t]))))
+        foreach (var option in types.Select(t => _serviceProvider.GetService(typeof(IOptions<>).MakeGenericType(t))).Distinct())
         {
             yield return option!.GetType().GetProperty("Value", BindingFlags.Public | BindingFlags.Instance)!.GetValue(option);
         }
