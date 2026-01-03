466dcd1d757b1947e8d912d8c47c260c6c3af687
thomhurst <30480171+thomhurst@users.noreply.github.com>
2026-01-03 12:28:20 +0000

    chore: Update winget CLI options

--- src/ModularPipelines.WinGet/Options/InfoOptions.cs
+++
@@ -1,8 +0,0 @@
-using System.Diagnostics.CodeAnalysis;
-using ModularPipelines.Attributes;
-
-namespace ModularPipelines.WinGet.Options;
-
-[ExcludeFromCodeCoverage]
-[CliSubCommand("info")]
-public record InfoOptions : WingetOptions;
