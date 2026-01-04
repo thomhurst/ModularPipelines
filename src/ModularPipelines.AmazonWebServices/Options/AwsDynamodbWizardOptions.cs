f72cea61f559a786f264d65adbdb1f8c154471c2
thomhurst <30480171+thomhurst@users.noreply.github.com>
2026-01-04 02:56:54 +0000

    chore: Update aws CLI options

--- src/ModularPipelines.AmazonWebServices/Options/AwsDynamodbWizardOptions.cs
+++
@@ -1,8 +0,0 @@
-using System.Diagnostics.CodeAnalysis;
-using ModularPipelines.Attributes;
-
-namespace ModularPipelines.AmazonWebServices.Options;
-
-[ExcludeFromCodeCoverage]
-[CliCommand("dynamodb", "wizard")]
-public record AwsDynamodbWizardOptions : AwsOptions;
