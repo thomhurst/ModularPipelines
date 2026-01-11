edb124d83477eb44c285736bb12502c4524c56a7
thomhurst <30480171+thomhurst@users.noreply.github.com>
2026-01-11 02:56:14 +0000

    chore: Update aws CLI options

--- src/ModularPipelines.AmazonWebServices/Options/AwsS3Options.cs
+++
@@ -1,8 +0,0 @@
-using System.Diagnostics.CodeAnalysis;
-using ModularPipelines.Attributes;
-
-namespace ModularPipelines.AmazonWebServices.Options;
-
-[ExcludeFromCodeCoverage]
-[CliSubCommand("s3")]
-public record AwsS3Options : AwsOptions;
