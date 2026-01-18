757b87ad6b386bf9e4bd4851efe7c1804b459442
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2026-01-18 03:07:27 +0000

    chore: Update newman CLI options (#2177)
    Co-authored-by: thomhurst <9139608+thomhurst@users.noreply.github.com>

--- src/ModularPipelines.Newman/Options/NewmanUrlOptions.Generated.cs
+++ src/ModularPipelines.Newman/Options/NewmanUrlOptions.Generated.cs
@@ -16,7 +16,7 @@ namespace ModularPipelines.Newman.Options;
 /// <summary>
 /// run [options] &lt;collection&gt;  Initiate a Postman Collection run from a given
 /// </summary>
-[GeneratedCode("ModularPipelines.OptionsGenerator", "1.0.0")]
+[GeneratedCode("ModularPipelines.OptionsGenerator", "2.42.227-alpha.0.981+55c59948e83059a5184db05c992047e177e72c4b")]
 [ExcludeFromCodeCoverage]
 [CliSubCommand("URL")]
 public record NewmanUrlOptions : NewmanOptions
