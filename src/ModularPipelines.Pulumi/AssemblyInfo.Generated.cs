56cc37ac7c10572a3de16ab0495949cf7965ea3e
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2026-01-18 15:26:22 +0000

    chore: Update pulumi CLI options (#2225)
    Co-authored-by: thomhurst <9139608+thomhurst@users.noreply.github.com>

--- src/ModularPipelines.Pulumi/AssemblyInfo.Generated.cs
+++ src/ModularPipelines.Pulumi/AssemblyInfo.Generated.cs
@@ -6,4 +6,4 @@
 using System.Reflection;
 
 [assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.Tool", "pulumi")]
-[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-01-18T13:53:39.3496335Z")]
+[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-01-18T15:12:21.2975782Z")]
