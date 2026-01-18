e8798a53ac377c089c6f5db37bff3d750c186b16
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2026-01-18 03:05:41 +0000

    chore: Update go CLI options (#2175)
    Co-authored-by: thomhurst <9139608+thomhurst@users.noreply.github.com>

--- src/ModularPipelines.Go/Options/GoBuildOptions.Generated.cs
+++ src/ModularPipelines.Go/Options/GoBuildOptions.Generated.cs
@@ -15,7 +15,7 @@ namespace ModularPipelines.Go.Options;
 /// <summary>
 /// Build compiles the packages named by the import paths,
 /// </summary>
-[GeneratedCode("ModularPipelines.OptionsGenerator", "1.0.0")]
+[GeneratedCode("ModularPipelines.OptionsGenerator", "2.42.227-alpha.0.981+55c59948e83059a5184db05c992047e177e72c4b")]
 [ExcludeFromCodeCoverage]
 [CliSubCommand("build")]
 public record GoBuildOptions : GoOptions
--- src/ModularPipelines.Go/Services/IGo.Generated.cs
+++ src/ModularPipelines.Go/Services/IGo.Generated.cs
@@ -13,7 +13,7 @@ namespace ModularPipelines.Go.Services;
 /// <summary>
 /// Generated interface for go CLI commands.
 /// </summary>
-[GeneratedCode("ModularPipelines.OptionsGenerator", "1.0.0")]
+[GeneratedCode("ModularPipelines.OptionsGenerator", "2.42.227-alpha.0.981+55c59948e83059a5184db05c992047e177e72c4b")]
 public partial interface IGo
 {
     #region Commands
