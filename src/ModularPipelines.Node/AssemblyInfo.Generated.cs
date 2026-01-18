e185560a503092f00434cc0a16893b7fa9a0fec1
thomhurst <9139608+thomhurst@users.noreply.github.com>
2026-01-18 13:54:33 +0000

    chore: Update pnpm CLI options

--- src/ModularPipelines.Node/AssemblyInfo.Generated.cs
+++ src/ModularPipelines.Node/AssemblyInfo.Generated.cs
@@ -6,4 +6,4 @@
 using System.Reflection;
 
 [assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.Tool", "pnpm")]
-[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-01-18T02:54:44.1296000Z")]
+[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-01-18T13:53:40.4649715Z")]
--- src/ModularPipelines.Node/Extensions/PnpmExtensions.Generated.cs
+++ src/ModularPipelines.Node/Extensions/PnpmExtensions.Generated.cs
@@ -16,7 +16,7 @@ namespace ModularPipelines.Node.Extensions;
 /// <summary>
 /// Generated extensions for registering pnpm services.
 /// </summary>
-[GeneratedCode("ModularPipelines.OptionsGenerator", "2.42.227-alpha.0.981+55c59948e83059a5184db05c992047e177e72c4b")]
+[GeneratedCode("ModularPipelines.OptionsGenerator", "")]
 public static class PnpmExtensions
 {
 #pragma warning disable CA2255
