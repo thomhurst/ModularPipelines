fdac6ec593925c4a12d7365d50566b0d6c752604
thomhurst <9139608+thomhurst@users.noreply.github.com>
2026-01-18 15:12:50 +0000

    chore: Update grype CLI options

--- src/ModularPipelines.Grype/AssemblyInfo.Generated.cs
+++ src/ModularPipelines.Grype/AssemblyInfo.Generated.cs
@@ -6,4 +6,4 @@
 using System.Reflection;
 
 [assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.Tool", "grype")]
-[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-01-18T13:53:27.4000181Z")]
+[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-01-18T15:11:53.2006453Z")]
