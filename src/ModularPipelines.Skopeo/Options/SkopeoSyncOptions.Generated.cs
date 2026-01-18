dd3f7d676349570969d37202e5fd7d843e1fbde4
thomhurst <9139608+thomhurst@users.noreply.github.com>
2026-01-18 02:55:39 +0000

    chore: Update skopeo CLI options

--- src/ModularPipelines.Skopeo/Options/SkopeoSyncOptions.Generated.cs
+++ src/ModularPipelines.Skopeo/Options/SkopeoSyncOptions.Generated.cs
@@ -15,7 +15,7 @@ namespace ModularPipelines.Skopeo.Options;
 /// <summary>
 /// Copy all the images from a SOURCE to a DESTINATION.
 /// </summary>
-[GeneratedCode("ModularPipelines.OptionsGenerator", "1.0.0")]
+[GeneratedCode("ModularPipelines.OptionsGenerator", "2.42.227-alpha.0.981+55c59948e83059a5184db05c992047e177e72c4b")]
 [ExcludeFromCodeCoverage]
 [CliSubCommand("sync")]
 public record SkopeoSyncOptions : SkopeoOptions
