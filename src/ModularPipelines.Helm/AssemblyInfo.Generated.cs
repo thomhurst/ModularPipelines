c0195a317e07e636db3439f2ec39dbfde4488f2e
thomhurst <9139608+thomhurst@users.noreply.github.com>
2026-01-18 15:13:08 +0000

    chore: Update helm CLI options

--- src/ModularPipelines.Helm/AssemblyInfo.Generated.cs
+++ src/ModularPipelines.Helm/AssemblyInfo.Generated.cs
@@ -6,4 +6,4 @@
 using System.Reflection;
 
 [assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.Tool", "helm")]
-[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-01-18T13:58:41.5858693Z")]
+[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-01-18T15:12:12.9290897Z")]
