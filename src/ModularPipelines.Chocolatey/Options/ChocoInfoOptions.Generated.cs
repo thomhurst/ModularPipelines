b92010c76cd13a97e60ae759b4fbc7372da4d504
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2026-01-18 03:24:28 +0000

    chore: Update choco CLI options (#2190)
    Co-authored-by: thomhurst <9139608+thomhurst@users.noreply.github.com>

--- src/ModularPipelines.Chocolatey/Options/ChocoInfoOptions.Generated.cs
+++ src/ModularPipelines.Chocolatey/Options/ChocoInfoOptions.Generated.cs
@@ -15,7 +15,7 @@ namespace ModularPipelines.Chocolatey.Options;
 /// <summary>
 /// NOTE: See scripting in the command reference (`choco --help`) for how to
 /// </summary>
-[GeneratedCode("ModularPipelines.OptionsGenerator", "1.0.0")]
+[GeneratedCode("ModularPipelines.OptionsGenerator", "2.42.227-alpha.0.981+55c59948e83059a5184db05c992047e177e72c4b")]
 [ExcludeFromCodeCoverage]
 [CliSubCommand("info")]
 public record ChocoInfoOptions : ChocoOptions
