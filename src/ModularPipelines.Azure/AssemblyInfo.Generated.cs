25c538d8028563c02bcb3379ac4abef11a27382c
thomhurst <9139608+thomhurst@users.noreply.github.com>
2026-01-25 03:29:09 +0000

    chore: Update az CLI options

--- src/ModularPipelines.Azure/AssemblyInfo.Generated.cs
+++ src/ModularPipelines.Azure/AssemblyInfo.Generated.cs
@@ -6,4 +6,4 @@
 using System.Reflection;
 
 [assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.Tool", "az")]
-[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-01-18T15:04:11.9539235Z")]
+[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-01-25T03:28:13.4550899Z")]
--- src/ModularPipelines.Azure/Options/AzMysqlFlexibleServerRestoreOptions.Generated.cs
+++ src/ModularPipelines.Azure/Options/AzMysqlFlexibleServerRestoreOptions.Generated.cs
@@ -73,7 +73,7 @@ public record AzMysqlFlexibleServerRestoreOptions : AzOptions
     public bool? PublicAccess { get; set; }
 
     /// <summary>
-    /// The point in time in UTC to restore from (ISO8601 format), e.g., 2017-04-26T02:10:00+00:00The default value is set to current time.  Default: 2026-01-18T14:43:07+00:00.
+    /// The point in time in UTC to restore from (ISO8601 format), e.g., 2017-04-26T02:10:00+00:00The default value is set to current time.  Default: 2026-01-25T03:07:49+00:00.
     /// </summary>
     [CliFlag("--restore-time")]
     public bool? RestoreTime { get; set; }
