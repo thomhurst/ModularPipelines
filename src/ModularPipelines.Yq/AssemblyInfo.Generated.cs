c6d47e02aab64a4f6f25f6a2af9f65924ac0b75a
thomhurst <9139608+thomhurst@users.noreply.github.com>
2026-01-25 02:58:25 +0000

    chore: Update yq CLI options

--- src/ModularPipelines.Yq/AssemblyInfo.Generated.cs
+++ src/ModularPipelines.Yq/AssemblyInfo.Generated.cs
@@ -6,4 +6,4 @@
 using System.Reflection;
 
 [assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.Tool", "yq")]
-[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-01-18T02:54:31.9586357Z")]
+[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-01-25T02:57:29.9864594Z")]
--- src/ModularPipelines.Yq/Services/IYq.Generated.cs
+++ src/ModularPipelines.Yq/Services/IYq.Generated.cs
@@ -13,7 +13,7 @@ namespace ModularPipelines.Yq.Services;
 /// <summary>
 /// Generated interface for yq CLI commands.
 /// </summary>
-[GeneratedCode("ModularPipelines.OptionsGenerator", "2.42.227-alpha.0.981+55c59948e83059a5184db05c992047e177e72c4b")]
+[GeneratedCode("ModularPipelines.OptionsGenerator", "")]
 public partial interface IYq
 {
     #region Commands
@@ -25,7 +25,7 @@ public partial interface IYq
     /// <param name="executionOptions">The execution configuration options.</param>
     /// <param name="cancellationToken">Cancellation token.</param>
     /// <returns>The command result.</returns>
-    Task<CommandResult> EvalAll(YqEvalAllOptions? options = default, CommandExecutionOptions? executionOptions = null, CancellationToken cancellationToken = default);
+    Task<CommandResult> EvalAll(YqEvalAllOptions options = default, CommandExecutionOptions executionOptions = null, CancellationToken cancellationToken = default);
 
     /// <summary>
     /// yq is a portable command-line data file processor (https://github.com/mikefarah/yq/)
@@ -34,7 +34,7 @@ public partial interface IYq
     /// <param name="executionOptions">The execution configuration options.</param>
     /// <param name="cancellationToken">Cancellation token.</param>
     /// <returns>The command result.</returns>
-    Task<CommandResult> Eval(YqEvalOptions? options = default, CommandExecutionOptions? executionOptions = null, CancellationToken cancellationToken = default);
+    Task<CommandResult> Eval(YqEvalOptions options = default, CommandExecutionOptions executionOptions = null, CancellationToken cancellationToken = default);
 
     #endregion
 }
