12e721f4efced9175ec3812cbfc0475f83ed951b
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2026-01-25 03:16:15 +0000

    chore: Update terraform CLI options (#2256)
    Co-authored-by: thomhurst <9139608+thomhurst@users.noreply.github.com>

--- src/ModularPipelines.Terraform/AssemblyInfo.Generated.cs
+++ src/ModularPipelines.Terraform/AssemblyInfo.Generated.cs
@@ -6,4 +6,4 @@
 using System.Reflection;
 
 [assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.Tool", "terraform")]
-[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-01-18T13:54:04.1430386Z")]
+[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-01-25T02:57:41.8560449Z")]
