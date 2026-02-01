095ad8f2bdd24148185542e4cc11e5eaab1b9eab
thomhurst <9139608+thomhurst@users.noreply.github.com>
2026-02-01 03:14:22 +0000

    chore: Update pnpm CLI options

--- src/ModularPipelines.Node/AssemblyInfo.Generated.cs
+++ src/ModularPipelines.Node/AssemblyInfo.Generated.cs
@@ -6,4 +6,4 @@
 using System.Reflection;
 
 [assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.Tool", "pnpm")]
-[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-01-25T02:57:38.5086682Z")]
+[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-02-01T03:13:25.9181647Z")]
