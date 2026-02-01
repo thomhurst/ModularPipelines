c485341213f0adff2340dd20a7deb043e1314b34
thomhurst <9139608+thomhurst@users.noreply.github.com>
2026-02-01 03:13:53 +0000

    chore: Update podman CLI options

--- src/ModularPipelines.Podman/AssemblyInfo.Generated.cs
+++ src/ModularPipelines.Podman/AssemblyInfo.Generated.cs
@@ -6,4 +6,4 @@
 using System.Reflection;
 
 [assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.Tool", "podman")]
-[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-01-25T02:57:36.8120347Z")]
+[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-02-01T03:12:57.3368364Z")]
