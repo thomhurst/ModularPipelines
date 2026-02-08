479f9178ed5c78f5088535d4738043327bcf09ae
thomhurst <9139608+thomhurst@users.noreply.github.com>
2026-02-08 03:13:10 +0000

    chore: Update syft CLI options

--- src/ModularPipelines.Syft/AssemblyInfo.Generated.cs
+++ src/ModularPipelines.Syft/AssemblyInfo.Generated.cs
@@ -6,4 +6,4 @@
 using System.Reflection;
 
 [assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.Tool", "syft")]
-[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-01-25T02:58:39.9162997Z")]
+[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-02-08T03:12:16.0282684Z")]
