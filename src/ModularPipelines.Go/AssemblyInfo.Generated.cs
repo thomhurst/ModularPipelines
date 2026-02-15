b529f36624fd1fdbb6780e22eda470217928fed6
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2026-02-15 03:22:54 +0000

    chore: Update go CLI options (#2381)
    Co-authored-by: thomhurst <9139608+thomhurst@users.noreply.github.com>

--- src/ModularPipelines.Go/AssemblyInfo.Generated.cs
+++ src/ModularPipelines.Go/AssemblyInfo.Generated.cs
@@ -6,4 +6,4 @@
 using System.Reflection;
 
 [assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.Tool", "go")]
-[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-02-08T03:12:16.4444935Z")]
+[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-02-15T03:12:06.6760225Z")]
