4cacaf5cc53a5eb8bc1144a5be6b402f5c5ae1af
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2026-02-15 03:42:55 +0000

    chore: Update winget CLI options (#2398)
    Co-authored-by: thomhurst <9139608+thomhurst@users.noreply.github.com>

--- src/ModularPipelines.WinGet/AssemblyInfo.Generated.cs
+++ src/ModularPipelines.WinGet/AssemblyInfo.Generated.cs
@@ -6,4 +6,4 @@
 using System.Reflection;
 
 [assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.Tool", "winget")]
-[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-02-08T03:13:10.0243082Z")]
+[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-02-15T03:13:32.9264480Z")]
