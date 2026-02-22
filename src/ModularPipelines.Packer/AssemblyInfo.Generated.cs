f4ae554335bcda49a5fa87d739e4f8a7818efb84
thomhurst <9139608+thomhurst@users.noreply.github.com>
2026-02-22 03:11:58 +0000

    chore: Update packer CLI options

--- src/ModularPipelines.Packer/AssemblyInfo.Generated.cs
+++ src/ModularPipelines.Packer/AssemblyInfo.Generated.cs
@@ -6,4 +6,4 @@
 using System.Reflection;
 
 [assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.Tool", "packer")]
-[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-02-08T03:12:36.5813588Z")]
+[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-02-22T03:11:03.2815041Z")]
