5b9c53f25d8a884830e913bbb00c4a59f3069143
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2026-02-22 03:29:49 +0000

    chore: Update newman CLI options (#2420)
    Co-authored-by: thomhurst <9139608+thomhurst@users.noreply.github.com>

--- src/ModularPipelines.Newman/AssemblyInfo.Generated.cs
+++ src/ModularPipelines.Newman/AssemblyInfo.Generated.cs
@@ -6,4 +6,4 @@
 using System.Reflection;
 
 [assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.Tool", "newman")]
-[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-02-15T03:13:36.7751277Z")]
+[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-02-22T03:13:01.0642949Z")]
