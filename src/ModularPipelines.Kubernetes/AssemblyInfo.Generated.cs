88b59bbadb316b2ef9b7764dcc16c01c795ba02a
thomhurst <9139608+thomhurst@users.noreply.github.com>
2026-02-22 03:11:33 +0000

    chore: Update kustomize CLI options

--- src/ModularPipelines.Kubernetes/AssemblyInfo.Generated.cs
+++ src/ModularPipelines.Kubernetes/AssemblyInfo.Generated.cs
@@ -6,4 +6,4 @@
 using System.Reflection;
 
 [assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.Tool", "kustomize")]
-[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-02-15T03:11:57.0151177Z")]
+[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-02-22T03:10:33.1221059Z")]
