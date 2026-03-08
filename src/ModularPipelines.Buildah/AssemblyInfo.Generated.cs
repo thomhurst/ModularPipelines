9aac103d426cf8e07b705442991f85fadd4cb07e
thomhurst <9139608+thomhurst@users.noreply.github.com>
2026-03-08 03:10:02 +0000

    chore: Update buildah CLI options

--- src/ModularPipelines.Buildah/AssemblyInfo.Generated.cs
+++ src/ModularPipelines.Buildah/AssemblyInfo.Generated.cs
@@ -6,4 +6,4 @@
 using System.Reflection;
 
 [assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.Tool", "buildah")]
-[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-02-15T03:12:11.5760233Z")]
+[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-03-08T03:09:04.1460500Z")]
