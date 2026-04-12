5bd56a0f4cf8e79ffa363eb00414abfcf86e818e
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2026-04-12 04:45:01 +0100

    chore: Update grype CLI options (#2465)
    Co-authored-by: thomhurst <9139608+thomhurst@users.noreply.github.com>

--- src/ModularPipelines.Grype/Services/GrypeDb.Generated.cs
+++ src/ModularPipelines.Grype/Services/GrypeDb.Generated.cs
@@ -85,6 +85,21 @@ public class GrypeDb
     }
 
     /// <summary>
+    /// Diff two databases, showing packages with added, removed, and modified vulnerability matches
+    /// </summary>
+    /// <param name="options">The command options.</param>
+    /// <param name="executionOptions">The execution configuration options.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>The command result.</returns>
+    public virtual async Task<CommandResult> Diff(
+        GrypeDbDiffOptions options = default,
+        CommandExecutionOptions executionOptions = null,
+        CancellationToken cancellationToken = default)
+    {
+        return await _command.ExecuteCommandLineTool(options ?? new GrypeDbDiffOptions(), executionOptions, cancellationToken);
+    }
+
+    /// <summary>
     /// import a vulnerability database archive from a local FILE or URL.
     /// </summary>
     /// <param name="options">The command options.</param>
