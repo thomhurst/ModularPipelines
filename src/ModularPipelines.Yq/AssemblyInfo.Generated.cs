128f1f3137a68220b2b6a360ab2956631c9247e8
thomhurst <9139608+thomhurst@users.noreply.github.com>
2026-04-26 03:27:28 +0000

    chore: Update yq CLI options

--- src/ModularPipelines.Yq/AssemblyInfo.Generated.cs
+++ src/ModularPipelines.Yq/AssemblyInfo.Generated.cs
@@ -6,4 +6,4 @@
 using System.Reflection;
 
 [assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.Tool", "yq")]
-[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-04-19T03:37:02.4614566Z")]
+[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-04-26T03:26:03.4316897Z")]
