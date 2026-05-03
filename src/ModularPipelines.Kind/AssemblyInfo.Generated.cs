b94e097f10886fc0083b0a2f096cfdff661f7e71
thomhurst <9139608+thomhurst@users.noreply.github.com>
2026-05-03 03:43:12 +0000

    chore: Update kind CLI options

--- src/ModularPipelines.Kind/AssemblyInfo.Generated.cs
+++ src/ModularPipelines.Kind/AssemblyInfo.Generated.cs
@@ -6,4 +6,4 @@
 using System.Reflection;
 
 [assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.Tool", "kind")]
-[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-04-26T03:17:56.9310420Z")]
+[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-05-03T03:41:50.0575364Z")]
