44cf40d0400e16773ac16b28aa46435099b2f31d
thomhurst <9139608+thomhurst@users.noreply.github.com>
2026-05-03 03:44:40 +0000

    chore: Update minikube CLI options

--- src/ModularPipelines.Minikube/AssemblyInfo.Generated.cs
+++ src/ModularPipelines.Minikube/AssemblyInfo.Generated.cs
@@ -6,4 +6,4 @@
 using System.Reflection;
 
 [assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.Tool", "minikube")]
-[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-04-26T03:18:18.8642789Z")]
+[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-05-03T03:43:03.4516766Z")]
