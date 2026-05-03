bd8fdff19e21e21dd2f1a4b7d3256be9cda81267
thomhurst <9139608+thomhurst@users.noreply.github.com>
2026-05-03 03:19:18 +0000

    chore: Update flux CLI options

--- src/ModularPipelines.Flux/AssemblyInfo.Generated.cs
+++ src/ModularPipelines.Flux/AssemblyInfo.Generated.cs
@@ -6,4 +6,4 @@
 using System.Reflection;
 
 [assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.Tool", "flux")]
-[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-04-26T03:20:31.1103368Z")]
+[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-05-03T03:17:39.9426254Z")]
