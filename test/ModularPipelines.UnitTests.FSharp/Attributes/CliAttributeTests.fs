2a4ceaf0b7ecfe85c127c1a87024b857fa0a1c05
James Alickolli <jalickolli@gmail.com>
2026-05-13 19:56:44 +1000

    Update CliAttributeTests.fs

--- test/ModularPipelines.UnitTests.FSharp/Attributes/CliAttributeTests.fs
+++ test/ModularPipelines.UnitTests.FSharp/Attributes/CliAttributeTests.fs
@@ -6,7 +6,7 @@ open TUnit.Assertions
 open TUnit.Assertions.Extensions
 open TUnit.Assertions.FSharp.Operations
 
-type CliAttributeTests =
+type CliAttributeTests() =
     member private this.ModelProvider = CommandModelProvider()
     member private this.ArgumentBuilder = CommandArgumentBuilder()
     member private this.BuildArguments(optionsObject: obj) =
@@ -19,3 +19,26 @@ type CliAttributeTests =
         let parts: string array = attribute.GetAllParts();
         do! check(Assert.That<string array>(parts).IsEquivalentTo([| "helm"; "install" |]))
     }
+    
+    [<Test>]
+    member _.CliCommand_Returns_Only_Tool_When_No_SubCommands() = async {
+        let attribute = new CliCommandAttribute("helm");
+        let parts: string array = attribute.GetAllParts();
+        do! check(Assert.That<string array>(parts).IsEquivalentTo([| "helm" |]))
+    }
+
+    [<Test>]
+    member _.CliCommand_Returns_Multiple_SubCommands() = async {
+        let attribute = new CliCommandAttribute("kubectl", "get", "pods");
+        let parts: string array = attribute.GetAllParts();
+        do! check(Assert.That<string array>(parts).IsEquivalentTo([| "kubectl"; "get"; "pods" |]))
+    }
+
+    [<Test>]
+    member _.CliFlag_Returns_Name_When_ShortForm_Not_Preferred() = async {
+        let attribute = CliFlagAttribute("--debug")
+        attribute.ShortForm <- "-d"
+        do! check(StringEqualsAssertionExtensions.IsEqualTo(Assert.That(attribute.GetEffectiveName()), "--debug"))
+    }
+
+   
