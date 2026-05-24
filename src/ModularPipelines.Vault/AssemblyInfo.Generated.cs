9ff84380cee60d5f0791c2648074380fa5e97692
thomhurst <9139608+thomhurst@users.noreply.github.com>
2026-05-24 03:21:50 +0000

    chore: Update vault CLI options

--- src/ModularPipelines.Vault/AssemblyInfo.Generated.cs
+++ src/ModularPipelines.Vault/AssemblyInfo.Generated.cs
@@ -6,4 +6,4 @@
 using System.Reflection;
 
 [assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.Tool", "vault")]
-[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-05-17T03:19:27.0222563Z")]
+[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-05-24T03:20:46.2245978Z")]
