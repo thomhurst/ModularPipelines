46d0d798bf61bc39420ba8ea1856b624490c327c
thomhurst <9139608+thomhurst@users.noreply.github.com>
2026-06-21 03:25:51 +0000

    chore: Update pip CLI options

--- src/ModularPipelines.Python/AssemblyInfo.Generated.cs
+++ src/ModularPipelines.Python/AssemblyInfo.Generated.cs
@@ -6,4 +6,4 @@
 using System.Reflection;
 
 [assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.Tool", "pip")]
-[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-06-14T03:44:15.1785427Z")]
+[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-06-21T03:25:00.7744255Z")]
