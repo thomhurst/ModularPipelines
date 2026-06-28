463d3c7aa402f2475e0b63724ef735fd5539713b
thomhurst <9139608+thomhurst@users.noreply.github.com>
2026-06-28 03:33:42 +0000

    chore: Update cargo CLI options

--- src/ModularPipelines.Rust/AssemblyInfo.Generated.cs
+++ src/ModularPipelines.Rust/AssemblyInfo.Generated.cs
@@ -6,4 +6,4 @@
 using System.Reflection;
 
 [assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.Tool", "cargo")]
-[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-06-21T03:41:57.8459833Z")]
+[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-06-28T03:32:47.9437682Z")]
