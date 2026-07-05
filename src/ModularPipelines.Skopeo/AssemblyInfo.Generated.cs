030c7813fd37a3dc1a7d8027ad38c9945b777d3b
thomhurst <9139608+thomhurst@users.noreply.github.com>
2026-07-05 03:16:30 +0000

    chore: Update skopeo CLI options

--- src/ModularPipelines.Skopeo/AssemblyInfo.Generated.cs
+++ src/ModularPipelines.Skopeo/AssemblyInfo.Generated.cs
@@ -6,4 +6,4 @@
 using System.Reflection;
 
 [assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.Tool", "skopeo")]
-[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-06-28T03:20:19.8619060Z")]
+[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-07-05T03:15:40.3774027Z")]
