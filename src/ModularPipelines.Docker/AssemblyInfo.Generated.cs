98be55aca90c5991f3e039faddcaf790e26f9cd0
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2026-07-12 04:20:37 +0100

    chore: Update docker CLI options (#2916)
    Co-authored-by: thomhurst <9139608+thomhurst@users.noreply.github.com>

--- src/ModularPipelines.Docker/AssemblyInfo.Generated.cs
+++ src/ModularPipelines.Docker/AssemblyInfo.Generated.cs
@@ -6,4 +6,4 @@
 using System.Reflection;
 
 [assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.Tool", "docker")]
-[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-07-05T03:15:39.3106653Z")]
+[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-07-12T03:12:11.3282483Z")]
