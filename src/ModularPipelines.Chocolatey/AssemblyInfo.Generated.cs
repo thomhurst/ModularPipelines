4ae92b22feb465076844e8581b994fdd6e7200fc
thomhurst <9139608+thomhurst@users.noreply.github.com>
2026-07-19 03:14:16 +0000

    chore: Update choco CLI options

--- src/ModularPipelines.Chocolatey/AssemblyInfo.Generated.cs
+++ src/ModularPipelines.Chocolatey/AssemblyInfo.Generated.cs
@@ -6,4 +6,4 @@
 using System.Reflection;
 
 [assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.Tool", "choco")]
-[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-07-12T03:13:51.2038259Z")]
+[assembly: AssemblyMetadata("ModularPipelines.OptionsGenerator.GeneratedAt", "2026-07-19T03:13:13.0843205Z")]
