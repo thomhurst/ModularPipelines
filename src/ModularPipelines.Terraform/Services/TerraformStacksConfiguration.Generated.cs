3e6f49293951044e0201c68fd8574d1fa15ccf5a
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2026-07-19 04:45:51 +0100

    chore: Update terraform CLI options (#2973)
    Co-authored-by: thomhurst <9139608+thomhurst@users.noreply.github.com>

--- src/ModularPipelines.Terraform/Services/TerraformStacksConfiguration.Generated.cs
+++ src/ModularPipelines.Terraform/Services/TerraformStacksConfiguration.Generated.cs
@@ -62,6 +62,21 @@ public class TerraformStacksConfiguration
     }
 
     /// <summary>
+    /// Show the details of a configuration. This command will display the current
+    /// </summary>
+    /// <param name="options">The command options.</param>
+    /// <param name="executionOptions">The execution configuration options.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>The command result.</returns>
+    public virtual async Task<CommandResult> Show(
+        TerraformStacksConfigurationShowOptions options = default,
+        CommandExecutionOptions executionOptions = null,
+        CancellationToken cancellationToken = default)
+    {
+        return await _command.ExecuteCommandLineTool(options ?? new TerraformStacksConfigurationShowOptions(), executionOptions, cancellationToken);
+    }
+
+    /// <summary>
     /// Upload the configuration in the current directory
     /// </summary>
     /// <param name="options">The command options.</param>
--- src/ModularPipelines.Terraform/Services/TerraformStacksDeploymentRun.Generated.cs
+++ src/ModularPipelines.Terraform/Services/TerraformStacksDeploymentRun.Generated.cs
@@ -77,6 +77,21 @@ public class TerraformStacksDeploymentRun
     }
 
     /// <summary>
+    /// Show the details of a single deployment run, including its deployment steps.
+    /// </summary>
+    /// <param name="options">The command options.</param>
+    /// <param name="executionOptions">The execution configuration options.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>The command result.</returns>
+    public virtual async Task<CommandResult> Show(
+        TerraformStacksDeploymentRunShowOptions options = default,
+        CommandExecutionOptions executionOptions = null,
+        CancellationToken cancellationToken = default)
+    {
+        return await _command.ExecuteCommandLineTool(options ?? new TerraformStacksDeploymentRunShowOptions(), executionOptions, cancellationToken);
+    }
+
+    /// <summary>
     /// Watch the progress of a deployment run in real-time.
     /// </summary>
     /// <param name="options">The command options.</param>
