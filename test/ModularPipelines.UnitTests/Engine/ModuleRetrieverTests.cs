6dc6c983c2bd7107439f76f8ffb8fb4752017093
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2026-07-28 16:58:05 +0200

    perf(engine): remove estimate rate limit (#3361)

---
+++ test/ModularPipelines.UnitTests/Engine/ModuleRetrieverTests.cs
@@ -0,0 +1,61 @@
+using ModularPipelines.Engine;
+using ModularPipelines.Engine.Attributes;
+using ModularPipelines.Engine.Dependencies;
+using ModularPipelines.Modules;
+using Moq;
+
+namespace ModularPipelines.UnitTests.Engine;
+
+public class ModuleRetrieverTests
+{
+    [Test]
+    public async Task EstimatedTimeLookups_AreNotRateLimited()
+    {
+        var modules = Enumerable.Range(0, 101)
+            .Select(_ => Mock.Of<IModule>())
+            .ToArray();
+        var conditionHandler = new Mock<IModuleConditionHandler>();
+        conditionHandler
+            .Setup(x => x.ShouldIgnore(It.IsAny<IModule>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync((false, null));
+        var registrationEventExecutor = new Mock<IRegistrationEventExecutor>();
+        registrationEventExecutor
+            .Setup(x => x.InvokeRegistrationEventsAsync(It.IsAny<IEnumerable<IModule>>()))
+            .Returns(Task.CompletedTask);
+        var estimatedTimeProvider = new Mock<ISafeModuleEstimatedTimeProvider>();
+        var releaseLookups = new TaskCompletionSource(TaskCreationOptions.RunContinuationsAsynchronously);
+        var allLookupsStarted = new TaskCompletionSource(TaskCreationOptions.RunContinuationsAsynchronously);
+        var startedLookupCount = 0;
+        estimatedTimeProvider
+            .Setup(x => x.GetModuleEstimatedTimeAsync(It.IsAny<Type>()))
+            .Returns(async () =>
+            {
+                if (Interlocked.Increment(ref startedLookupCount) == modules.Length)
+                {
+                    allLookupsStarted.TrySetResult();
+                }
+
+                await releaseLookups.Task;
+                return TimeSpan.Zero;
+            });
+        estimatedTimeProvider
+            .Setup(x => x.GetSubModuleEstimatedTimesAsync(It.IsAny<Type>()))
+            .ReturnsAsync([]);
+        var retriever = new ModuleRetriever(
+            conditionHandler.Object,
+            registrationEventExecutor.Object,
+            modules,
+            estimatedTimeProvider.Object,
+            Mock.Of<IModuleDependencyRegistry>(),
+            Mock.Of<IModuleMetadataRegistry>());
+
+        var organizedModulesTask = retriever.GetOrganizedModules();
+        var allStartedBeforeRelease = await Task.WhenAny(
+            allLookupsStarted.Task,
+            Task.Delay(TimeSpan.FromSeconds(2))) == allLookupsStarted.Task;
+        releaseLookups.TrySetResult();
+        await organizedModulesTask;
+
+        await Assert.That(allStartedBeforeRelease).IsTrue();
+    }
+}
