e1998dbb5bf8b837fefbc284c9d6a836a229d090
thomhurst <9139608+thomhurst@users.noreply.github.com>
2026-08-02 03:15:39 +0000

    chore: Update choco CLI options

--- src/ModularPipelines.Chocolatey/Options/ChocoOptions.Generated.cs
+++ src/ModularPipelines.Chocolatey/Options/ChocoOptions.Generated.cs
@@ -19,6 +19,7 @@ namespace ModularPipelines.Chocolatey.Options;
 [GeneratedCode("ModularPipelines.OptionsGenerator", "2.0.0")]
 [ExcludeFromCodeCoverage]
 [CliTool("choco")]
+[CliGlobalOptions]
 public abstract record ChocoOptions : CommandLineToolOptions
 {
 }
