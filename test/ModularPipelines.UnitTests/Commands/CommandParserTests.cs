e8f622c63a7ab5b0ac627adb947f7b8eff78c641
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2026-08-02 15:50:02 +0100

    test(commands): expect obfuscated result input

--- test/ModularPipelines.UnitTests/Commands/CommandParserTests.cs
+++ test/ModularPipelines.UnitTests/Commands/CommandParserTests.cs
@@ -180,7 +180,7 @@ public class CommandParserTests : TestBase
             Version = "1.0.0",
             ApiKey = "my-secret-key"
         });
-        await Assert.That(result.CommandInput).IsEqualTo("dotnet nuget delete MyPackageName 1.0.0 --api-key my-secret-key");
+        await Assert.That(result.CommandInput).IsEqualTo("dotnet nuget delete MyPackageName 1.0.0 --api-key **********");
     }
 
     private async Task<CommandResult> GetResult(CommandLineToolOptions options)
