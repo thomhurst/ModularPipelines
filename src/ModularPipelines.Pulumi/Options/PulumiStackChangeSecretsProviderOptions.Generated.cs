764db6cb4f4dcd789eb59f274ec233c99037bbd0
thomhurst <9139608+thomhurst@users.noreply.github.com>
2026-08-03 01:10:11 +0000

    chore: Update pulumi CLI options

--- src/ModularPipelines.Pulumi/Options/PulumiStackChangeSecretsProviderOptions.Generated.cs
+++ src/ModularPipelines.Pulumi/Options/PulumiStackChangeSecretsProviderOptions.Generated.cs
@@ -19,7 +19,7 @@ namespace ModularPipelines.Pulumi.Options;
 [ExcludeFromCodeCoverage]
 [CliSubCommand("stack", "change-secrets-provider")]
 public record PulumiStackChangeSecretsProviderOptions(
-    [property: SecretValue, CliArgument(0, Placement = ArgumentPlacement.BeforeOptions)] string NewSecretsProvider
+    [property: CliArgument(0, Placement = ArgumentPlacement.BeforeOptions)] string NewSecretsProvider
 ) : PulumiOptions
 {
     /// <summary>
