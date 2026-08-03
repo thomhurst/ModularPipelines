766d5ecff47c6055e939fb3130d516df2d787144
thomhurst <9139608+thomhurst@users.noreply.github.com>
2026-08-03 11:52:04 +0000

    chore: Update podman CLI options

--- src/ModularPipelines.Podman/Options/PodmanInspectOptions.Generated.cs
+++ src/ModularPipelines.Podman/Options/PodmanInspectOptions.Generated.cs
@@ -19,7 +19,7 @@ namespace ModularPipelines.Podman.Options;
 [ExcludeFromCodeCoverage]
 [CliSubCommand("inspect")]
 public record PodmanInspectOptions(
-    [property: CliArgument(0)] string Container
+    [property: CliArgument(0)] IEnumerable<string> Container
 ) : PodmanOptions
 {
     /// <summary>
