df7916c79b9abb734530562a52c473bf20e5d6c1
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2026-08-09 16:28:23 +0100

    chore: Update grype CLI options (#3895)
    Co-authored-by: thomhurst <9139608+thomhurst@users.noreply.github.com>

--- src/ModularPipelines.Grype/Options/GrypeDbSearchOptions.Generated.cs
+++ src/ModularPipelines.Grype/Options/GrypeDbSearchOptions.Generated.cs
@@ -116,10 +116,4 @@ public record GrypeDbSearchOptions : GrypeOptions
     [CliOption("--verbose", ShortForm = "-v", Format = OptionFormat.EqualsSeparated)]
     public int? Verbose { get; set; }
 
-    /// <summary>
-    /// The command operand.
-    /// </summary>
-    [CliArgument(0, Phase = CommandLinePhase.EarlyOperand)]
-    public string? Command { get; set; }
-
 }
