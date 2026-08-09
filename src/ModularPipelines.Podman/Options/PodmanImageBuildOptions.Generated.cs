bbc511c64e61f5f3debf92d085ba5c4f002cbf74
thomhurst <9139608+thomhurst@users.noreply.github.com>
2026-08-09 02:52:12 +0000

    chore: Update podman CLI options

--- src/ModularPipelines.Podman/Options/PodmanImageBuildOptions.Generated.cs
+++ src/ModularPipelines.Podman/Options/PodmanImageBuildOptions.Generated.cs
@@ -9,6 +9,7 @@ using System.CodeDom.Compiler;
 using System.Diagnostics.CodeAnalysis;
 using ModularPipelines.Attributes;
 using ModularPipelines.Podman.Options;
+using ModularPipelines.Models;
 
 namespace ModularPipelines.Podman.Options;
 
@@ -408,7 +409,7 @@ public record PodmanImageBuildOptions : PodmanOptions
     /// Pull image policy ("always/true"|"missing"|"never/false"|"newer") (default "missing")
     /// </summary>
     [CliOption("--pull", Format = OptionFormat.EqualsSeparated, ValueArity = CliOptionValueArity.Optional)]
-    public string? Pull { get; set; }
+    public CliOptionValue? Pull { get; set; }
 
     /// <summary>
     /// refrain from announcing build instructions and image read/write progress
