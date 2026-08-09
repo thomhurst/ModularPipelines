70510929d5e291211a4e9449ddd5da5993862cfb
thomhurst <9139608+thomhurst@users.noreply.github.com>
2026-08-09 02:50:04 +0000

    chore: Update grype CLI options

--- src/ModularPipelines.Grype/Options/GrypeDbOptions.Generated.cs
+++ src/ModularPipelines.Grype/Options/GrypeDbOptions.Generated.cs
@@ -50,10 +50,4 @@ public record GrypeDbOptions : GrypeOptions
     [CliOption("--verbose", ShortForm = "-v", Format = OptionFormat.EqualsSeparated)]
     public int? Verbose { get; set; }
 
-    /// <summary>
-    /// The command operand.
-    /// </summary>
-    [CliArgument(0, Phase = CommandLinePhase.EarlyOperand)]
-    public string? Command { get; set; }
-
 }
