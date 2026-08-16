5e717c08aca5c5ddfa396bde9dc518be729a0747
thomhurst <9139608+thomhurst@users.noreply.github.com>
2026-08-16 02:36:09 +0000

    chore: Update grype CLI options

--- src/ModularPipelines.Grype/Services/IGrype.Generated.cs
+++ src/ModularPipelines.Grype/Services/IGrype.Generated.cs
@@ -36,7 +36,8 @@ public partial interface IGrype
     /// <param name="executionOptions">The execution configuration options.</param>
     /// <param name="cancellationToken">Cancellation token.</param>
     /// <returns>The command result.</returns>
-    Task<CommandResult> ExplainAsync(GrypeExplainOptions? options = null, CommandExecutionOptions? executionOptions = null, CancellationToken cancellationToken = default);
+    Task<CommandResult> ExplainAsync(GrypeExplainOptions? options = null, CommandExecutionOptions? executionOptions = null, CancellationToken cancellationToken = default)
+        => throw new System.NotSupportedException();
 
     #endregion
 }
