ee5fae1a55ec87d88277f9498003114f90593c05
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2026-08-16 14:59:06 +0100

    fix(podman): preserve regenerated option behavior (#3970)

--- src/ModularPipelines.Podman/Options/PodmanKubeDownOptions.Generated.cs
+++ src/ModularPipelines.Podman/Options/PodmanKubeDownOptions.Generated.cs
@@ -27,9 +27,9 @@ public record PodmanKubeDownOptions : PodmanOptions
     public bool? Force { get; set; }
 
     /// <summary>
-    /// The [KUBEFILE [KUBEFILE...]] operand.
+    /// The KUBEFILE operand.
     /// </summary>
     [CliArgument(0, Phase = CommandLinePhase.Passthrough)]
-    public string? KubefileKubefile { get; set; }
+    public IEnumerable<string>? Kubefile { get; set; }
 
 }
