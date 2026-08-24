6ff7908fd8bb573478361f810100a92e57f253b9
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2026-08-24 08:56:08 +0100

    fix(generator): retain passive repeatability

--- tools/ModularPipelines.OptionsGenerator/src/ModularPipelines.OptionsGenerator/Scrapers/Cli/CliScraperBase.cs
+++ tools/ModularPipelines.OptionsGenerator/src/ModularPipelines.OptionsGenerator/Scrapers/Cli/CliScraperBase.cs
@@ -1083,7 +1083,7 @@ public abstract partial class CliScraperBase : ICliScraper
         + @"|\A" + RepeatableItemCountPattern
         + @"|(?:can|may|must|should)\s+be\s+"
         + @"(?:specified|supplied|provided|used|passed|set|given)\s+"
-        + @"(?:multiple\s+times|more\s+than\s+once)"
+        + @"(?:(?:one|zero)\s+or\s+more\s+times|multiple\s+times|more\s+than\s+once)"
         + @"|(?:accepts?|specify|supply|provide|use|pass|set|give|supports?|takes?|contains?)\s+"
         + @"(?:multiple\s+times|more\s+than\s+once|"
         + RepeatableItemCountPattern
