c9f47c315cb26844cdf2aa894d8e799aa9cc9ac7
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2026-08-25 08:37:29 +0100

    chore: Update newman CLI options (#4094)
    Co-authored-by: thomhurst <9139608+thomhurst@users.noreply.github.com>

--- src/ModularPipelines.Newman/Services/INewman.Generated.cs
+++ src/ModularPipelines.Newman/Services/INewman.Generated.cs
@@ -27,7 +27,7 @@ public partial interface INewman
     /// <param name="executionOptions">The execution configuration options.</param>
     /// <param name="cancellationToken">Cancellation token.</param>
     /// <returns>The command result.</returns>
-    Task<CommandResult> RunAsync(NewmanRunOptions options, CommandExecutionOptions? executionOptions = null, CancellationToken cancellationToken = default)
+    public Task<CommandResult> RunAsync(NewmanRunOptions options, CommandExecutionOptions? executionOptions = null, CancellationToken cancellationToken = default)
         => throw new System.NotSupportedException();
 
     #endregion
