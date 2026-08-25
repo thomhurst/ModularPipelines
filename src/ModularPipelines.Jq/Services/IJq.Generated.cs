2da332f49d11d4a9387a0bf5ca292bb3095aae4a
thomhurst <9139608+thomhurst@users.noreply.github.com>
2026-08-25 03:20:59 +0000

    chore: Update jq CLI options

--- src/ModularPipelines.Jq/Services/IJq.Generated.cs
+++ src/ModularPipelines.Jq/Services/IJq.Generated.cs
@@ -27,7 +27,7 @@ public partial interface IJq
     /// <param name="executionOptions">The execution configuration options.</param>
     /// <param name="cancellationToken">Cancellation token.</param>
     /// <returns>The command result.</returns>
-    Task<CommandResult> ExecuteAsync(JqExecuteOptions? options = null, CommandExecutionOptions? executionOptions = null, CancellationToken cancellationToken = default)
+    public Task<CommandResult> ExecuteAsync(JqExecuteOptions? options = null, CommandExecutionOptions? executionOptions = null, CancellationToken cancellationToken = default)
         => throw new System.NotSupportedException();
 
     #endregion
