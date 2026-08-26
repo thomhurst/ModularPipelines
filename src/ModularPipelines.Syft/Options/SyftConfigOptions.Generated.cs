6bd46424c25eda0733d28963add050b9b0c3de24
thomhurst <9139608+thomhurst@users.noreply.github.com>
2026-08-26 02:02:45 +0000

    chore: Update syft CLI options

--- src/ModularPipelines.Syft/Options/SyftConfigOptions.Generated.cs
+++ src/ModularPipelines.Syft/Options/SyftConfigOptions.Generated.cs
@@ -56,4 +56,10 @@ public record SyftConfigOptions : SyftOptions
     [CliOption("--verbose", ShortForm = "-v", Format = OptionFormat.EqualsSeparated)]
     public int? Verbose { get; set; }
 
+    /// <summary>
+    /// The command operand.
+    /// </summary>
+    [CliArgument(0, Phase = CommandLinePhase.EarlyOperand)]
+    public string? Command { get; set; }
+
 }
