20dc633ab5d4c38cc0648656ca827a2c75df206d
thomhurst <9139608+thomhurst@users.noreply.github.com>
2026-08-30 02:29:29 +0000

    chore: Update argocd CLI options

--- src/ModularPipelines.ArgoCd/Options/ArgoCdProjRemoveSignatureKeyOptions.Generated.cs
+++ src/ModularPipelines.ArgoCd/Options/ArgoCdProjRemoveSignatureKeyOptions.Generated.cs
@@ -15,6 +15,7 @@ namespace ModularPipelines.ArgoCd.Options;
 
 [GeneratedCode("ModularPipelines.OptionsGenerator", "2.0.0")]
 [ExcludeFromCodeCoverage]
+[Obsolete("This command is no longer supported by the installed CLI and is retained only for compatibility.")]
 [CliSubCommand("proj", "remove-signature-key")]
 public record ArgoCdProjRemoveSignatureKeyOptions(
     [property: CliArgument(0, Phase = CommandLinePhase.EarlyOperand, Required = true)] string Project,
