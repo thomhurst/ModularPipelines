b0f4e78b7ff15ca242803a01cbd9b1ab202d4907
thomhurst <9139608+thomhurst@users.noreply.github.com>
2026-08-31 21:03:34 +0000

    chore: Update jq CLI options

--- src/ModularPipelines.Jq/Extensions/JqExtensions.Generated.cs
+++ src/ModularPipelines.Jq/Extensions/JqExtensions.Generated.cs
@@ -9,7 +9,6 @@ using System.CodeDom.Compiler;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using ModularPipelines.Attributes;
-using ModularPipelines.Context;
 using ModularPipelines.Jq.Services;
 
 namespace ModularPipelines.Jq.Extensions;
@@ -31,4 +30,5 @@ public static class JqExtensions
         services.TryAddScoped<IJq, Services.Jq>();
         return services;
     }
+
 }
