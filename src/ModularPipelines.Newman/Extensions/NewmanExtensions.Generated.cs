02f3dda3706beacb6bd7276f766745a38ec88aee
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2026-08-31 23:53:08 +0100

    chore: Update newman CLI options (#4429)
    Co-authored-by: thomhurst <9139608+thomhurst@users.noreply.github.com>

--- src/ModularPipelines.Newman/Extensions/NewmanExtensions.Generated.cs
+++ src/ModularPipelines.Newman/Extensions/NewmanExtensions.Generated.cs
@@ -9,7 +9,6 @@ using System.CodeDom.Compiler;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using ModularPipelines.Attributes;
-using ModularPipelines.Context;
 using ModularPipelines.Newman.Services;
 
 namespace ModularPipelines.Newman.Extensions;
@@ -31,4 +30,5 @@ public static class NewmanExtensions
         services.TryAddScoped<INewman, Services.Newman>();
         return services;
     }
+
 }
