820f721ac1adaff3739561ff2b8c0e9aef0d3c5f
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2026-09-01 13:28:54 +0100

    fix(go): preserve list option semantics

--- test/ModularPipelines.Go.UnitTests/GoOptionsTests.cs
+++ test/ModularPipelines.Go.UnitTests/GoOptionsTests.cs
@@ -46,4 +46,30 @@ public class GoOptionsTests : TestBase
 
         await Assert.That(commandLine.ToString()).IsEqualTo("go test ./pkg -args payload -test.v");
     }
+
+    [Test]
+    public async Task List_Json_Renders_Bare_Value()
+    {
+        var builder = await GetService<ICommandLineBuilder>();
+
+        var commandLine = builder.Build(new GoListOptions
+        {
+            Json = CliOptionValue.Bare,
+        });
+
+        await Assert.That(commandLine.ToString()).IsEqualTo("go list -json");
+    }
+
+    [Test]
+    public async Task List_Json_Renders_Selected_Fields()
+    {
+        var builder = await GetService<ICommandLineBuilder>();
+
+        var commandLine = builder.Build(new GoListOptions
+        {
+            Json = "ImportPath,Name",
+        });
+
+        await Assert.That(commandLine.ToString()).IsEqualTo("go list -json=ImportPath,Name");
+    }
 }
