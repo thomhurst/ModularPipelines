3832785cb33e25572fcb1e047edf5befad002772
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2026-09-01 13:43:03 +0100

    chore: Update pip CLI options (#4485)
    Co-authored-by: thomhurst <9139608+thomhurst@users.noreply.github.com>

--- src/ModularPipelines.Python/Options/PipInstallOptions.Generated.cs
+++ src/ModularPipelines.Python/Options/PipInstallOptions.Generated.cs
@@ -357,7 +357,7 @@ public record PipInstallOptions : PipOptions
     public string? UseDeprecated { get; set; }
 
     /// <summary>
-    /// The requirement specifier operand.
+    /// The &lt;requirement specifier&gt; operand.
     /// </summary>
     [CliArgument(0, Phase = CommandLinePhase.Passthrough)]
     public IEnumerable<string>? RequirementSpecifier { get; set; }
