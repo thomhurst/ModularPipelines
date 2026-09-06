834fbae84a24dc7cfe387000e9dafe09d8812d21
thomhurst <9139608+thomhurst@users.noreply.github.com>
2026-09-06 14:14:00 +0000

    chore: Update kubectl CLI options

--- src/ModularPipelines.Kubernetes/Enums/KubernetesReplaceCascade.Generated.cs
+++ src/ModularPipelines.Kubernetes/Enums/KubernetesReplaceCascade.Generated.cs
@@ -19,9 +19,9 @@ public enum KubernetesReplaceCascade
     [EnumValue("background")]
     Background,
 
-    [EnumValue("orphan")]
-    Orphan,
-
     [EnumValue("foreground")]
-    Foreground
+    Foreground,
+
+    [EnumValue("orphan")]
+    Orphan
 }
