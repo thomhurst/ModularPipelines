34bae9c9efd10fa78fc87412ca562454bcf20b8d
thomhurst <9139608+thomhurst@users.noreply.github.com>
2026-09-11 19:51:31 +0000

    chore: Update kubectl CLI options

--- src/ModularPipelines.Kubernetes/Enums/KubernetesKubercSetPolicy.Generated.cs
+++ src/ModularPipelines.Kubernetes/Enums/KubernetesKubercSetPolicy.Generated.cs
@@ -19,9 +19,9 @@ public enum KubernetesKubercSetPolicy
     [EnumValue("AllowAll")]
     AllowAll,
 
-    [EnumValue("DenyAll")]
-    DenyAll,
-
     [EnumValue("Allowlist")]
-    Allowlist
+    Allowlist,
+
+    [EnumValue("DenyAll")]
+    DenyAll
 }
