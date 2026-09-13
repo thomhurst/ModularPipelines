da2246ad3dc55e5e471f5c1eb5fb7f99d08a2aa8
thomhurst <9139608+thomhurst@users.noreply.github.com>
2026-09-13 16:06:39 +0000

    chore: Update terraform CLI options
    (cherry picked from commit c40c04e626f0a2ad727a65e058848a741c3cb1ae)

--- src/ModularPipelines.Terraform/Options/TerraformStacksDeploymentRunWatchOptions.Generated.cs
+++ src/ModularPipelines.Terraform/Options/TerraformStacksDeploymentRunWatchOptions.Generated.cs
@@ -18,12 +18,8 @@ namespace ModularPipelines.Terraform.Options;
 [GeneratedCode("ModularPipelines.OptionsGenerator", "2.0.0")]
 [ExcludeFromCodeCoverage]
 [CliSubCommand("stacks", "deployment-run", "watch")]
-public record TerraformStacksDeploymentRunWatchOptions : TerraformOptions
+public record TerraformStacksDeploymentRunWatchOptions(
+    [property: CliOption("-deployment-run-id", Format = OptionFormat.EqualsSeparated)] string DeploymentRunId
+) : TerraformOptions
 {
-    /// <summary>
-    /// The ID of the deployment run to watch (required).
-    /// </summary>
-    [CliOption("-deployment-run-id", Format = OptionFormat.EqualsSeparated)]
-    public string? DeploymentRunId { get; set; }
-
 }
