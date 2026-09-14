2a872a8269ef97a7e3efe039f16646b02eeb5cf8
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2026-09-14 01:56:35 +0100

    chore: Update syft CLI options (#4900)
    Co-authored-by: thomhurst <9139608+thomhurst@users.noreply.github.com>

--- src/ModularPipelines.Syft/Services/SyftCataloger.Generated.cs
+++ src/ModularPipelines.Syft/Services/SyftCataloger.Generated.cs
@@ -44,7 +44,7 @@ public class SyftCataloger : ISyftCataloger
         CommandExecutionOptions? executionOptions = null,
         CancellationToken cancellationToken = default)
     {
-        return await _command.ExecuteCommandLineToolAsync(options ?? new SyftCatalogerOptions(), executionOptions, cancellationToken);
+        return await _command.ExecuteCommandLineToolAsync(options ?? new SyftCatalogerOptions(), executionOptions, cancellationToken).ConfigureAwait(false);
     }
 
     /// <summary>
@@ -59,7 +59,7 @@ public class SyftCataloger : ISyftCataloger
         CommandExecutionOptions? executionOptions = null,
         CancellationToken cancellationToken = default)
     {
-        return await _command.ExecuteCommandLineToolAsync(options ?? new SyftCatalogerListOptions(), executionOptions, cancellationToken);
+        return await _command.ExecuteCommandLineToolAsync(options ?? new SyftCatalogerListOptions(), executionOptions, cancellationToken).ConfigureAwait(false);
     }
 
     #endregion
