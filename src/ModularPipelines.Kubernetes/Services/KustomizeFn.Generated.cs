fdbf7490acba4c3b2665076af40fa0fc43f1ba57
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2026-09-14 01:44:46 +0100

    chore: Update kustomize CLI options (#4890)
    Co-authored-by: thomhurst <9139608+thomhurst@users.noreply.github.com>

--- src/ModularPipelines.Kubernetes/Services/KustomizeFn.Generated.cs
+++ src/ModularPipelines.Kubernetes/Services/KustomizeFn.Generated.cs
@@ -44,7 +44,7 @@ public class KustomizeFn : IKustomizeFn
         CommandExecutionOptions? executionOptions = null,
         CancellationToken cancellationToken = default)
     {
-        return await _command.ExecuteCommandLineToolAsync(options ?? new KustomizeFnOptions(), executionOptions, cancellationToken);
+        return await _command.ExecuteCommandLineToolAsync(options ?? new KustomizeFnOptions(), executionOptions, cancellationToken).ConfigureAwait(false);
     }
 
     /// <summary>
@@ -59,7 +59,7 @@ public class KustomizeFn : IKustomizeFn
         CommandExecutionOptions? executionOptions = null,
         CancellationToken cancellationToken = default)
     {
-        return await _command.ExecuteCommandLineToolAsync(options ?? new KustomizeFnRunOptions(), executionOptions, cancellationToken);
+        return await _command.ExecuteCommandLineToolAsync(options ?? new KustomizeFnRunOptions(), executionOptions, cancellationToken).ConfigureAwait(false);
     }
 
     #endregion
