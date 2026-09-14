fec3239a1b5380fba1f3932866535617bfb839b4
thomhurst <9139608+thomhurst@users.noreply.github.com>
2026-09-14 11:02:46 +0000

    chore: Update helm CLI options

--- src/ModularPipelines.Helm/Services/HelmSearch.Generated.cs
+++ src/ModularPipelines.Helm/Services/HelmSearch.Generated.cs
@@ -44,7 +44,7 @@ public class HelmSearch : IHelmSearch
         CommandExecutionOptions? executionOptions = null,
         CancellationToken cancellationToken = default)
     {
-        return await _command.ExecuteCommandLineToolAsync(options ?? new HelmSearchOptions(), executionOptions, cancellationToken);
+        return await _command.ExecuteCommandLineToolAsync(options ?? new HelmSearchOptions(), executionOptions, cancellationToken).ConfigureAwait(false);
     }
 
     /// <summary>
@@ -59,7 +59,7 @@ public class HelmSearch : IHelmSearch
         CommandExecutionOptions? executionOptions = null,
         CancellationToken cancellationToken = default)
     {
-        return await _command.ExecuteCommandLineToolAsync(options ?? new HelmSearchHubOptions(), executionOptions, cancellationToken);
+        return await _command.ExecuteCommandLineToolAsync(options ?? new HelmSearchHubOptions(), executionOptions, cancellationToken).ConfigureAwait(false);
     }
 
     /// <summary>
@@ -74,7 +74,7 @@ public class HelmSearch : IHelmSearch
         CommandExecutionOptions? executionOptions = null,
         CancellationToken cancellationToken = default)
     {
-        return await _command.ExecuteCommandLineToolAsync(options ?? new HelmSearchRepoOptions(), executionOptions, cancellationToken);
+        return await _command.ExecuteCommandLineToolAsync(options ?? new HelmSearchRepoOptions(), executionOptions, cancellationToken).ConfigureAwait(false);
     }
 
     #endregion
