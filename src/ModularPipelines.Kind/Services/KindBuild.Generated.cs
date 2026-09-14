ef94bfa777dbba63763ab0fe97c22e86ae74d57a
thomhurst <9139608+thomhurst@users.noreply.github.com>
2026-09-14 00:21:02 +0000

    chore: Update kind CLI options

--- src/ModularPipelines.Kind/Services/KindBuild.Generated.cs
+++ src/ModularPipelines.Kind/Services/KindBuild.Generated.cs
@@ -44,7 +44,7 @@ public class KindBuild : IKindBuild
         CommandExecutionOptions? executionOptions = null,
         CancellationToken cancellationToken = default)
     {
-        return await _command.ExecuteCommandLineToolAsync(options ?? new KindBuildOptions(), executionOptions, cancellationToken);
+        return await _command.ExecuteCommandLineToolAsync(options ?? new KindBuildOptions(), executionOptions, cancellationToken).ConfigureAwait(false);
     }
 
     /// <summary>
@@ -59,7 +59,7 @@ public class KindBuild : IKindBuild
         CommandExecutionOptions? executionOptions = null,
         CancellationToken cancellationToken = default)
     {
-        return await _command.ExecuteCommandLineToolAsync(options ?? new KindBuildNodeImageOptions(), executionOptions, cancellationToken);
+        return await _command.ExecuteCommandLineToolAsync(options ?? new KindBuildNodeImageOptions(), executionOptions, cancellationToken).ConfigureAwait(false);
     }
 
     #endregion
--- src/ModularPipelines.Kind/Services/KindCreate.Generated.cs
+++ src/ModularPipelines.Kind/Services/KindCreate.Generated.cs
@@ -44,7 +44,7 @@ public class KindCreate : IKindCreate
         CommandExecutionOptions? executionOptions = null,
         CancellationToken cancellationToken = default)
     {
-        return await _command.ExecuteCommandLineToolAsync(options ?? new KindCreateOptions(), executionOptions, cancellationToken);
+        return await _command.ExecuteCommandLineToolAsync(options ?? new KindCreateOptions(), executionOptions, cancellationToken).ConfigureAwait(false);
     }
 
     /// <summary>
@@ -59,7 +59,7 @@ public class KindCreate : IKindCreate
         CommandExecutionOptions? executionOptions = null,
         CancellationToken cancellationToken = default)
     {
-        return await _command.ExecuteCommandLineToolAsync(options ?? new KindCreateClusterOptions(), executionOptions, cancellationToken);
+        return await _command.ExecuteCommandLineToolAsync(options ?? new KindCreateClusterOptions(), executionOptions, cancellationToken).ConfigureAwait(false);
     }
 
     #endregion
