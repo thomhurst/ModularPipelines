148ed71cb64f6e15ed79dad42950d879f4e91bc8
thomhurst <9139608+thomhurst@users.noreply.github.com>
2026-09-14 11:15:34 +0000

    chore: Update eksctl CLI options

--- src/ModularPipelines.Eksctl/Services/EksctlDrain.Generated.cs
+++ src/ModularPipelines.Eksctl/Services/EksctlDrain.Generated.cs
@@ -44,7 +44,7 @@ public class EksctlDrain : IEksctlDrain
         CommandExecutionOptions? executionOptions = null,
         CancellationToken cancellationToken = default)
     {
-        return await _command.ExecuteCommandLineToolAsync(options ?? new EksctlDrainOptions(), executionOptions, cancellationToken);
+        return await _command.ExecuteCommandLineToolAsync(options ?? new EksctlDrainOptions(), executionOptions, cancellationToken).ConfigureAwait(false);
     }
 
     /// <summary>
@@ -59,7 +59,7 @@ public class EksctlDrain : IEksctlDrain
         CommandExecutionOptions? executionOptions = null,
         CancellationToken cancellationToken = default)
     {
-        return await _command.ExecuteCommandLineToolAsync(options ?? new EksctlDrainNodegroupOptions(), executionOptions, cancellationToken);
+        return await _command.ExecuteCommandLineToolAsync(options ?? new EksctlDrainNodegroupOptions(), executionOptions, cancellationToken).ConfigureAwait(false);
     }
 
     #endregion
