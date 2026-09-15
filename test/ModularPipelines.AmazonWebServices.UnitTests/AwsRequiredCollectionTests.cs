4cc54351449664720ad8d6e454c6e42ecd46bd97
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2026-09-15 18:38:55 +0100

    test(aws): cover regenerated scalar and JSON input guards

--- test/ModularPipelines.AmazonWebServices.UnitTests/AwsRequiredCollectionTests.cs
+++ test/ModularPipelines.AmazonWebServices.UnitTests/AwsRequiredCollectionTests.cs
@@ -6,6 +6,28 @@ namespace ModularPipelines.AmazonWebServices.UnitTests;
 public class AwsRequiredCollectionTests
 {
     [Test]
+    [Arguments("")]
+    [Arguments(" \t\r\n")]
+    public async Task Alternate_Input_Factories_Reject_Blank_Json(string input)
+    {
+        await Assert.That(() => AwsEc2TerminateInstancesOptions.FromCliInputJson(input))
+            .Throws<ArgumentException>();
+        await Assert.That(() => AwsEc2CreateKeyPairOptions.FromCliInputJson(input))
+            .Throws<ArgumentException>();
+        await Assert.That(() => AwsAutoscalingSetInstanceProtectionOptions.FromCliInputJson(input))
+            .Throws<ArgumentException>();
+    }
+
+    [Test]
+    public async Task Required_Strings_Reject_Null()
+    {
+        await Assert.That(() => new AwsEc2CreateKeyPairOptions(null!))
+            .Throws<ArgumentNullException>();
+        await Assert.That(() => new AwsAutoscalingSetInstanceProtectionOptions(["i-example"], null!, true))
+            .Throws<ArgumentNullException>();
+    }
+
+    [Test]
     public async Task InstanceIds_Reject_A_Collection_Containing_Only_Null()
     {
         await Assert.That(() => new AwsEc2TerminateInstancesOptions([null!]))
