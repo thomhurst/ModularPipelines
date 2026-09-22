33141e2bd36d25d178f00d62a5413286e0586b23
thomhurst <9139608+thomhurst@users.noreply.github.com>
2026-09-22 12:14:43 +0000

    chore: Update snyk CLI options

--- src/ModularPipelines.Snyk/Enums/SnykFilePathGroup.Generated.cs
+++ src/ModularPipelines.Snyk/Enums/SnykFilePathGroup.Generated.cs
@@ -11,7 +11,7 @@ using ModularPipelines.Attributes;
 namespace ModularPipelines.Snyk.Enums;
 
 /// <summary>
-/// Allowed values for --file-path-group
+/// Allowed values for --file-path-group.
 /// </summary>
 [GeneratedCode("ModularPipelines.OptionsGenerator", "2.0.0")]
 public enum SnykFilePathGroup
